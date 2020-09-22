80f084adc91ba5598d3c8aaff63afcb094341d2b
Alistair Smith <alistairsmith01@gmail.com>
2020-09-22 15:22:46 +0100

    i'm stupid !

--- api/discord.ts
+++ api/discord.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NowRequest, res: NowResponse): Promis
 }
 
 function sendIssue(payload: IncomingLinearWebhookPayload, webhook: { id: string; token: string }) {
-  return fetch(`https://discord.com/api/webhooks/${webhook[0]}/${webhook[1]}`, {
+  return fetch(`https://discord.com/api/webhooks/${webhook.id}/${webhook.token}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
