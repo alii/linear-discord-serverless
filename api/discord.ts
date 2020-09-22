ec8d8f7dc7062de811a9270e02bc61365442cb10
Alistair Smith <alistairsmith01@gmail.com>
2020-09-22 15:37:13 +0100

    This works!

--- api/discord.ts
+++ api/discord.ts
@@ -23,45 +23,51 @@ export default async function handler(req: NowRequest, res: NowResponse): Promis
 }
 
 function sendIssue(payload: IncomingLinearWebhookPayload, webhook: { id: string; token: string }) {
-  return fetch(`https://discord.com/api/webhooks/${webhook.id}/${webhook.token}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      embeds: [
-        {
-          color: 0x4752b2,
-          author: {
-            name: `Issue Created [${getId(payload.url)}]`,
-          },
-          title: payload.data.title,
-          url: payload.url,
-          fields: [
-            {
-              name: 'Priority',
-              value: getPriorityValue(payload.data.priority || 0),
-              inline: true,
-            },
-            {
-              name: 'Points',
-              value: payload.data.estimate,
-              inline: true,
-            },
-            {
-              name: 'Labels',
-              value: prettifyLabels(payload.data.labels || []),
-              inline: false,
-            },
-          ],
-          timestamp: new Date(),
-          footer: {
-            text: `Linear App`,
-            icon_url: 'https://pbs.twimg.com/profile_images/1121592030449168385/MF6whgy1_400x400.png',
-          },
+  const url = `https://discord.com/api/webhooks/${webhook.id}/${webhook.token}`;
+
+  const embed = {
+    content: 'New Linear issue',
+    embeds: [
+      {
+        color: 0x4752b2,
+        author: {
+          name: `Issue Created [${getId(payload.url)}]`,
         },
-      ],
-    }),
+        title: payload.data.title,
+        url: payload.url,
+        fields: [
+          {
+            name: 'Priority',
+            value: getPriorityValue(payload.data.priority || 0),
+            inline: true,
+          },
+          {
+            name: 'Points',
+            value: payload.data.estimate || '0',
+            inline: true,
+          },
+          {
+            name: 'Labels',
+            value: prettifyLabels(payload.data.labels || []),
+            inline: false,
+          },
+        ],
+        timestamp: new Date(),
+        footer: {
+          text: `Linear App`,
+          icon_url: 'https://pbs.twimg.com/profile_images/1121592030449168385/MF6whgy1_400x400.png',
+        },
+      },
+    ],
+  };
+
+  const body = JSON.stringify(embed);
+  console.log({ body });
+
+  return fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body,
   });
 }
 
