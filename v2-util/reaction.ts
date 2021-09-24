b530290f7791adc14366a803db80f7fbdb4caa32
Alistair Smith <alistairsmith01@gmail.com>
2021-09-24 17:42:19 +0100

    feat: Reaction schema

--- v2-util/reaction.ts
+++ v2-util/reaction.ts
@@ -1,5 +1,26 @@
 import {z} from 'zod';
+import {createAllStates, dateSchema, defaultRemoveSchema} from './util';
 
-export const reaction = z.object({
-	type: z.literal('Reaction'),
+const commons = z.object({
+	id: z.string().uuid(),
+	createdAt: dateSchema,
+	updatedAt: dateSchema,
+	emoji: z.string(),
+	userId: z.string().uuid(),
+	commentId: z.string().uuid(),
+	comment: z.object({
+		id: z.string().uuid(),
+		body: z.string(),
+		userId: z.string().uuid(),
+	}),
+	user: z.object({
+		id: z.string().uuid(),
+		name: z.string(),
+	}),
 });
+
+export const reaction = createAllStates(commons, defaultRemoveSchema).and(
+	z.object({
+		type: z.literal('Reaction'),
+	}),
+);
