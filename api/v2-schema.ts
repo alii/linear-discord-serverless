2f9a7eb8b7e7b178709935d0d67b1b56c9155058
Alistair Smith <alistairsmith01@gmail.com>
2021-09-26 01:47:56 +0100

    remove troublesome schema route

--- api/v2-schema.ts
+++
@@ -1,9 +0,0 @@
-import {api} from 'nextkit';
-import zodToJsonSchema from 'zod-to-json-schema';
-import {bodySchema} from '../v2-util/schema';
-
-export default api({
-	async GET() {
-		return zodToJsonSchema(bodySchema, 'body');
-	},
-});
